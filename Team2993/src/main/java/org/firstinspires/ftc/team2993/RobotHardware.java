db73e2e728835ee1c174bc88684f8ec382741b67
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-09-21 15:52:48 -0400

    Initial Commit for 2993

---
+++ Team2993/src/main/java/org/firstinspires/ftc/team2993/RobotHardware.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.team2993;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+public class RobotHardware {
+
+    private HardwareMap map = null;
+
+    public DcMotor  fL = null, fR = null, bL = null, bR = null;    //DC Motors
+
+    private static final int       CPR = 1120;                                 //encoder counts per revolution
+    private static final double    DIAMETER = 4;                               //encoded drive wheel diameter (in)
+    private static final double    GEARING = 1;
+    public static final double     CPI = (CPR * GEARING) / (DIAMETER * 3.14);
+    public static final double     CPF = CPI * 12;
+
+    public RobotHardware(HardwareMap map){
+        this.map = map;
+    }
+
+    public void init() {
+
+        fL = map.get(DcMotor.class, "fL");
+        fR = map.get(DcMotor.class, "fR");
+        bL = map.get(DcMotor.class, "bL");
+        bR = map.get(DcMotor.class, "bR");
+
+        fL.setDirection(DcMotorSimple.Direction.FORWARD);
+        fR.setDirection(DcMotorSimple.Direction.REVERSE);
+        bL.setDirection(DcMotorSimple.Direction.FORWARD);
+        bR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    /*
+        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        resetEnc();
+        */
+    }
+
+    //Slaves left side motors and right side motors to each other to avoid chain problems
+    public void leftDrive(float n) {
+        fL.setPower(n);
+        bL.setPower(n);
+    }
+
+    public void rightDrive(float n) {
+        fR.setPower(n);
+        bR.setPower(n);
+    }
+
+    //Drives at a power
+    public void drive(int num) {
+        leftDrive(num);
+        rightDrive(num);
+    }
+
+    //Drives w/ input
+    public void drive(Gamepad gp) {
+        float turn = gp.right_stick_x / 4;
+        leftDrive(gp.left_stick_y + turn);
+        rightDrive(gp.left_stick_y - turn);
+    }
+
+    public void stop() {
+        drive(0);
+    }
+
+    public boolean driveIsBusy(){
+        return fL.isBusy() || fR.isBusy();
+    }
+
+    public void resetEnc(){
+        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+
+
+}
