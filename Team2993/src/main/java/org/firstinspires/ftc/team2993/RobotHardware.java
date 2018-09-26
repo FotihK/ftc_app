ff6b1211d08d287650c7327584a4fa70674848e5
Tyler Cosentino <tylcos@users.noreply.github.com>
2018-09-26 16:03:25 -0400

    Two wheel tank drive

--- Team2993/src/main/java/org/firstinspires/ftc/team2993/RobotHardware.java
+++ Team2993/src/main/java/org/firstinspires/ftc/team2993/RobotHardware.java
@@ -1,92 +1,58 @@
 package org.firstinspires.ftc.team2993;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.*;
 
 
 
-public class RobotHardware {
+public class RobotHardware
+{
+    public DcMotor l;
+    public DcMotor r;
 
-    private HardwareMap map = null;
+    private HardwareMap map;
 
-    public DcMotor  fL = null, fR = null, bL = null, bR = null;    //DC Motors
 
-    private static final int       CPR = 1120;                                 //encoder counts per revolution
-    private static final double    DIAMETER = 4;                               //encoded drive wheel diameter (in)
-    private static final double    GEARING = 1;
-    public static final double     CPI = (CPR * GEARING) / (DIAMETER * 3.14);
-    public static final double     CPF = CPI * 12;
 
-    public RobotHardware(HardwareMap map){
+    public RobotHardware(HardwareMap map)
+    {
         this.map = map;
-    }
 
-    public void init() {
-
-        fL = map.get(DcMotor.class, "fL");
-        fR = map.get(DcMotor.class, "fR");
-        bL = map.get(DcMotor.class, "bL");
-        bR = map.get(DcMotor.class, "bR");
-
-        fL.setDirection(DcMotorSimple.Direction.FORWARD);
-        fR.setDirection(DcMotorSimple.Direction.REVERSE);
-        bL.setDirection(DcMotorSimple.Direction.FORWARD);
-        bR.setDirection(DcMotorSimple.Direction.REVERSE);
-
-    /*
-        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        resetEnc();
-        */
-    }
-
-    //Slaves left side motors and right side motors to each other to avoid chain problems
-    public void leftDrive(float n) {
-        fL.setPower(n);
-        bL.setPower(n);
-    }
-
-    public void rightDrive(float n) {
-        fR.setPower(n);
-        bR.setPower(n);
-    }
-
-    //Drives at a power
-    public void drive(int num) {
-        leftDrive(num);
-        rightDrive(num);
-    }
-
-    //Drives w/ input
-    public void drive(Gamepad gp) {
-        float turn = gp.right_stick_x / 4;
-        leftDrive(gp.left_stick_y + turn);
-        rightDrive(gp.left_stick_y - turn);
-    }
-
-    public void stop() {
-        drive(0);
-    }
-
-    public boolean driveIsBusy(){
-        return fL.isBusy() || fR.isBusy();
-    }
-
-    public void resetEnc(){
-        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        fL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        fR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        init();
     }
 
 
 
+    public void init()
+    {
+        l = map.get(DcMotor.class, "l");
+        r = map.get(DcMotor.class, "r");
+
+        l.setDirection(DcMotorSimple.Direction.FORWARD);
+        r.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+
+
+    public void Drive(double power)
+    {
+        Drive(power, power);
+    }
+
+    public void Drive(double lPower, double rPower)
+    {
+        l.setPower(lPower);
+        r.setPower(rPower);
+    }
+
+
+
+    public void Stop()
+    {
+        Drive(0d);
+    }
+
+    public boolean DriveIsBusy()
+    {
+        return l.isBusy() || r.isBusy();
+    }
 }
--- Team2993/src/main/java/org/firstinspires/ftc/team2993/TankDrive.java
+++ Team2993/src/main/java/org/firstinspires/ftc/team2993/TankDrive.java
@@ -1,24 +1,45 @@
-package org.firstinspires.ftc.team3819;
+package org.firstinspires.ftc.team2993;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.team3819.RobotHardware;
+import com.qualcomm.robotcore.eventloop.opmode.*;
 
 
 
 @TeleOp(name="team2993")
-public class TankDrive extends OpMode {
+public class TankDrive extends OpMode
+{
     RobotHardware robot;
 
+    public final double threshold = .1d;
+
+
+
     @Override
-    public void init() {
+    public void init()
+    {
         robot = new RobotHardware(hardwareMap);
-        robot.init();
     }
 
-    private void driverOne() {
-        robot.drive(gamepad1);
+
+
+    private void driverOne()
+    {
+        double y = gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+
+        if (Math.abs(y) < threshold)
+            y = 0;
+        if (Math.abs(x) < threshold)
+            x = 0;
+
+        double l = y + x;
+        double r = y - x;
+
+        if (Math.abs(l) > 1d)
+            l = 1d * Math.signum(l);
+        if (Math.abs(r) > 1d)
+            r = 1d * Math.signum(r);
+
+        robot.Drive(l, x);
     }
 
 
@@ -29,3 +50,6 @@ public class TankDrive extends OpMode {
         driverOne();
     }
 }
+
+
+
