27605ded4b40968aaf62c787b33dda15c53328fe
tameem-asif <tameem.asif@gsmst.org>
2018-11-10 09:19:53 -0500

    fotih xd

--- Team2981/src/main/java/org/firstinspires/ftc/team2981/systems/RobotDrive.java
+++ Team2981/src/main/java/org/firstinspires/ftc/team2981/systems/RobotDrive.java
@@ -26,23 +26,26 @@ public class RobotDrive extends TankDrive {
 
     public static final PIDFCoefficients NORMAL_PID = new PIDFCoefficients(20, 8, 12, 0);
 
-    private DcMotorEx left, right;
+    private DcMotorEx left1, left2, right1, right2;
     private List<DcMotorEx> motors;
 
     public RobotDrive(HardwareMap map){
         super(TRACK_WIDTH);
 
-        left = map.get(DcMotorEx.class, "MotorL");
-        right = map.get(DcMotorEx.class, "MotorR");
+        left1 = map.get(DcMotorEx.class, "L1");
+        right1 = map.get(DcMotorEx.class, "R1");
+        left2 = map.get(DcMotorEx.class, "L2");
+        right2 = map.get(DcMotorEx.class, "R2");
 
-        motors = Arrays.asList(left, right);
+        motors = Arrays.asList(left1, left2, right1, right2);
 
         for(DcMotorEx motor : motors){
             motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             motor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, NORMAL_PID);
         }
 
-        right.setDirection(DcMotorSimple.Direction.REVERSE);
+        right1.setDirection(DcMotorSimple.Direction.REVERSE);
+        right2.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     private static double ticksToInches(int ticks){
@@ -62,7 +65,9 @@ public class RobotDrive extends TankDrive {
 
     @Override
     public void setMotorPowers(double v1, double v2) {
-        left.setPower(v1);
-        right.setPower(v2);
+        left1.setPower(v1);
+        left2.setPower(v1);
+        right1.setPower(v2);
+        right2.setPower(v2);
     }
 }
